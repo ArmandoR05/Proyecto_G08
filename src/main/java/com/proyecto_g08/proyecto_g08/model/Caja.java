e52571f6f4af85aa77b0ae94927e78b1a56a10d2
ArmandoR05 <130397409+ArmandoR05@users.noreply.github.com>
2025-03-26 18:28:53 -0600

    a

--- src/main/java/com/proyecto_g08/proyecto_g08/model/Caja.java
+++ src/main/java/com/proyecto_g08/proyecto_g08/model/Caja.java
@@ -2,11 +2,8 @@ package com.proyecto_g08.proyecto_g08.model;
 
 
 public class Caja {
-    private int id;
     private String tipo;
     private boolean estado;
 
-    public void atenderCliente() {}
-    public void agregarCliente(Tiquete tiquete) {}
-    public int obtenerClientesEnCola() { return 0; }
+
 }
